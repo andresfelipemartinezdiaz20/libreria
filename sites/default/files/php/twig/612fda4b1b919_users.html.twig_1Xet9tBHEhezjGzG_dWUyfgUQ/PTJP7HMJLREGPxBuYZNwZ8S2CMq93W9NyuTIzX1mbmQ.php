98284e75d1d8fdbc27ab56923c7532fe80a6e92e
Andres Martinez <andresmartinez@Andress-Mac-mini.local>
2021-10-03 17:48:13 -0500

    primer commit

---
+++ sites/default/files/php/twig/612fda4b1b919_users.html.twig_1Xet9tBHEhezjGzG_dWUyfgUQ/PTJP7HMJLREGPxBuYZNwZ8S2CMq93W9NyuTIzX1mbmQ.php
@@ -0,0 +1,113 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/drupal_up_controller/templates/users.html.twig */
+class __TwigTemplate_cfd1b1b5817443625f35bc4ce567b42bb7fea4cf99710a15470702fd4bc98c1e extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 11, "for" => 15];
+        $filters = ["escape" => 14];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 11
+        $context["classes"] = [0 => "clear-both"];
+        // line 14
+        echo "<div";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo ">
+    ";
+        // line 15
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["content"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["value"]) {
+            // line 16
+            echo "        <p>";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($context["value"]), "html", null, true);
+            echo "</p>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['value'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 18
+        echo "</div>
+<div class=\"description\"> 
+    ";
+        // line 20
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description"] ?? null)), "html", null, true);
+        echo "
+</div>
+
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/drupal_up_controller/templates/users.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  79 => 20,  75 => 18,  66 => 16,  62 => 15,  57 => 14,  55 => 11,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/drupal_up_controller/templates/users.html.twig", "/var/www/html/modules/drupal_up_controller/templates/users.html.twig");
+    }
+}
---
+++ sites/default/settings.local.php
@@ -0,0 +1,13 @@
+<?php
+
+$settings['hash_salt'] = '123456567890';
+$databases['default']['default'] = array (
+'database' => 'drupal',
+'username' => 'admin',
+'password' => 'admin',
+'host' => 'mariadb',
+'port' => '3306',
+'driver' => 'mysql',
+'prefix' => '',
+);
+// $config_directories['sync'] = '../config/sync';
