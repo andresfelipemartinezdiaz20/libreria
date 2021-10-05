89ac89f4ded5ec199ec03fdb90315e89e2dfeae0
Andres Martinez <andresmartinez@Andress-Mac-mini.local>
2021-10-05 01:20:19 -0500

    drupal

---
+++ sites/default/settings.local.php
@@ -0,0 +1,13 @@
+<?php
+$settings['hash_salt'] =  '123456567890';
+$databases['default']['default'] = array (
+  'database' => 'biblioteca',
+  'username' => 'admin',
+  'password' => 'admin',
+  'host' => 'mariadb',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+);
+
+$settings['config_sync_directory'] = '../config/sync';
